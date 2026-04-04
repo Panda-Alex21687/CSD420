07f38806e94e3414893c9ee9a64d3de81026eaca
Panda-Alex21687 <Alex21687@gmail.com>
2026-04-04 00:43:50 -0500

    Add

--- Module 2/Module 2.2 Assignment/demo/src/main/java/com/baldree/mod2/RandomDataRecord.java
+++
@@ -1,9 +0,0 @@
-package com.baldree.mod2;
-
-/**
- * Stores one record of data from the file:
- * - five integers
- * - five doubles
- */
-public record RandomDataRecord(int[] integers, double[] doubles) {
-}
