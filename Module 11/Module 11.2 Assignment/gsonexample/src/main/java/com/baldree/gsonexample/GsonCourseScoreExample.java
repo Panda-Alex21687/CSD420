a732ae348e85d43a0a6e55f22e7ee90ca3b7c9e1
Panda-Alex21687 <Alex21687@gmail.com>
2026-05-20 15:06:18 -0500

    add mod 11.2

--- Module 11/Module 11.2 Assignment/gsonexample/src/main/java/com/baldree/gsonexample/GsonCourseScoreExample.java
+++ Module 11/Module 11.2 Assignment/gsonexample/src/main/java/com/baldree/gsonexample/GsonCourseScoreExample.java
@@ -9,72 +9,72 @@ import com.google.gson.reflect.TypeToken;
 
 public class GsonCourseScoreExample {
 
-    public static void main(String[] args) {
-        String jsonData = """
-                [
-                  {
-                    "studentName": "Alex",
-                    "course": "CSD420",
-                    "module": 11,
-                    "score": 96.5,
-                    "passed": true
-                  },
-                  {
-                    "studentName": "Jordan",
-                    "course": "CSD420",
-                    "module": 11,
-                    "score": 88.0,
-                    "passed": true
-                  }
-                ]
-                """;
+  public static void main(String[] args) {
+    String jsonData = """
+        [
+          {
+            "studentName": "Alex",
+            "course": "CSD420",
+            "module": 11,
+            "score": 96.5,
+            "passed": true
+          },
+          {
+            "studentName": "Jordan",
+            "course": "CSD420",
+            "module": 11,
+            "score": 88.0,
+            "passed": true
+          }
+        ]
+        """;
 
-        Gson gson = new GsonBuilder()
-                .setPrettyPrinting()
-                .create();
+    Gson gson = new GsonBuilder()
+        .setPrettyPrinting()
+        .create();
 
-        Type scoreListType = new TypeToken<List<CourseScore>>() {
-        }.getType();
+    Type scoreListType = new TypeToken<List<CourseScore>>() {
+    }.getType();
 
-        List<CourseScore> scores = gson.fromJson(jsonData, scoreListType);
+    List<CourseScore> scores = gson.fromJson(jsonData, scoreListType);
 
-        System.out.println("Original JSON converted into Java objects:");
+    System.out.println("Original JSON converted into Java objects:");
 
-        for (CourseScore score : scores) {
-            System.out.println(
-                    score.studentName
-                            + " earned "
-                            + score.score
-                            + " in "
-                            + score.course
-                            + " Module "
-                            + score.module
-                            + ". Passed: "
-                            + score.passed);
-        }
-
-        scores.add(new CourseScore("Taylor", "CSD420", 11, 91.0, true));
-
-        String updatedJson = gson.toJson(scores);
-
-        System.out.println();
-        System.out.println("Updated Java list converted back into JSON:");
-        System.out.println(updatedJson);
+    for (CourseScore score : scores) {
+      System.out.println(
+          score.studentName
+              + " earned "
+              + score.score
+              + " in "
+              + score.course
+              + " Module "
+              + score.module
+              + ". Passed: "
+              + score.passed);
     }
 
-    static class CourseScore {
-        String studentName;
-        String course;
-        int module;
-        double score;
-        boolean passed;
+    scores.add(new CourseScore("Taylor", "CSD420", 11, 91.0, true));
 
-        CourseScore(String studentName, String course, int module, double score, boolean passed) {
-            this.studentName = studentName;
-            this.course = course;
-            this.module = module;
-            this.score = score;
-            this.passed = passed;
-        }
+    String updatedJson = gson.toJson(scores);
+
+    System.out.println();
+    System.out.println("Updated Java list converted back into JSON:");
+    System.out.println(updatedJson);
+  }
+
+  static class CourseScore {
+    String studentName;
+    String course;
+    int module;
+    double score;
+    boolean passed;
+
+    CourseScore(String studentName, String course, int module, double score, boolean passed) {
+      this.studentName = studentName;
+      this.course = course;
+      this.module = module;
+      this.score = score;
+      this.passed = passed;
     }
+  }
 }
